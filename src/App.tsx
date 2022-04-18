ebc226196ce968af8de5d41b2967c746635538c6
Adam Doskhoev <Adam.Doskhoev@glidewelldental.com>
2022-04-18 23:28:35 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "mobx-react";
 
 function App() {
   return (
+    // <Provider store={store}>
+    //   <BrowserRouter>
+    //     <Header />
+    //     <Switch>
+    //       <Route exact={true} path="/">
+    //         <Home store={store} />
+    //       </Route>
+    //       <Route path="/about" component={About} />
+    //       <Route path="/order" component={Order} />
+    //       <Route path="/contacts" component={Contacts} />
+    //     </Switch>
+    //   </BrowserRouter>
+    // </Provider>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
